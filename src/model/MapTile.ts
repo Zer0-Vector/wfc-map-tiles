90ce02ed6278ab50eee5d4469653cdcc8917fa1b
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2025-11-02 03:40:20 -0500

    refactor: replace edges type with TileEdgeMap

--- src/model/MapTile.ts
+++ src/model/MapTile.ts
@@ -3,9 +3,13 @@ import type { EdgeType } from "./EdgeType";
 export type CardinalDirection = "N" | "S" | "E" | "W";
 export type Direction = CardinalDirection | (string & {});
 
+export type TileEdgeMap = { 
+  [key in CardinalDirection]: EdgeType
+} & Record<string, EdgeType>;
+
 export interface MapTile {
   id: string;
   name?: string;
-  edges: Record<Direction, EdgeType>;
+  edges: TileEdgeMap;
   fitsWith(tile: MapTile, edgeType: EdgeType): boolean;
 }
