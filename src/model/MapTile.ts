699d1c811004897e5a64e7bf6a859fe2191aee8e
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2025-11-04 06:00:52 -0500

    refactor(CardinalDirection): enhance representation and structure

--- src/model/MapTile.ts
+++ src/model/MapTile.ts
@@ -1,11 +1,31 @@
 import type { EdgeType } from "./EdgeType";
 
-export type CardinalDirection = "N" | "S" | "E" | "W";
+export const CardinalDirectionValues = ["N", "E", "S", "W"] as const;
+export type CardinalDirection = typeof CardinalDirectionValues[number];
+export const CardinalDirection = {
+  N: {
+    opposite: "S",
+    offset: { x: 0, y: -1 }
+  },
+  E: {
+    opposite: "W",
+    offset: { x: 1, y: 0 }
+  },
+  S: {
+    opposite: "N",
+    offset: { x: 0, y: 1 }
+  },
+  W: {
+    opposite: "E",
+    offset: { x: -1, y: 0 }
+  }
+} as const;
+
 export type Direction = CardinalDirection | (string & {});
 
 export type TileEdgeMap = { 
   [key in CardinalDirection]: EdgeType
-} & Record<string, EdgeType>;
+};
 
 export interface MapTile {
   id: string;
